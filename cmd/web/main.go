0bc1ef1f95236e8a7c3e7982c335fe0fa68620ff
Udbhav <udbhav.rrohatgi96@gmail.com>
2024-08-12 17:49:06 +0530

    chapter 4: SQL setup and SQL database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,30 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"flag"
 	"os"
+
+	"snippetbox/internal/models"
+	// "models"
+
+	_"github.com/go-sql-driver/mysql"
 )
 
 
 type application struct{
 	errorLog *log.Logger
 	infoLog *log.Logger
+	snippets *models.SnippetModel
 }
 
 
 func main(){
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+	
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog: infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
@@ -33,8 +50,19 @@ func main(){
 		Handler: app.routes(),
 	}
 
-	infoLog.Println("Starting Server on Port %s", *addr)
-	err := srv.ListenAndServe()
+	infoLog.Printf("Starting Server on Port %s", *addr)
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 
+}
+
+func openDB(dsn string) (*sql.DB, error){
+	db, err := sql.Open("mysql", dsn)
+	if err != nil{
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil 
 }
